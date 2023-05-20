4fc25e27f1fb0f0cc77e432070c048a142addf55
TheElementGuy <80215501+TheElementGuy@users.noreply.github.com>
2023-05-19 21:59:44 -0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,19 @@
+const sampleText = document.getElementByID("sample-text");
+function randomIntFromInterval(min, max) {
+	return Math.floor(Math.random() * (max - min + 1) + min);
+}
+async function loadFileAndReturn(url) {
+  try {
+    const response = await fetch(url);
+    const data = await response.text();
+    return data;
+  } catch (err) {
+    console.error(err);
+  }
+}
+function fillSampleText() {
+	switch(randomIntFromInterval(1, 1)) {
+		case 1:
+			sampleText.innerHTML = loadFileAndReturn("https://jpermsolves.github.io/coding-quiz/html.txt");
+	}
+}
